2dd1214d8b1efba24c34c6f3f62d2546a6bd11d6
Ulrike E <rika087@yahoo.de>
2015-07-06 10:03:12 +0200

    update

--- biete_check.php
+++ biete_check.php
@@ -2,22 +2,14 @@
 include "header.php";
 ?>
 
+<!-- content -->
 <div class="wrapper row2">
-    <div id="container" class="clear">
+    <div id="container" class="clear">>
             <section id="services" class="clear">
                 <article class="three_third">
-                    Ein Projekt zum Modul Web-Programmierung
-
-                    Fachhochschule Brandenburg
-                    Onlinestudiengang Medieninformatik
-
-                    SoSe 2015
-
-
-                    Ein Projekt von
-
-                    Ulrike Exner
-
+                    <?php
+                    include("process_biete_form.php");
+                    ?>
                 </article>
             </section>
     </div>
